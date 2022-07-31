a3e06afb939b96ca700fb7036f850f97e613eebd
Elaine Santos <elaineleonidossantos@gmail.com>
2022-07-30 23:29:52 -0300

    ex5

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,25 @@
+/*var c = 1
+while ( c < 6 ) {
+    console.log ('Tudo bem?')
+    c++
+}*/
+//-----------------------------
+/*var c = 1
+do {
+    console.log (`Passo ${C}`)
+    c++
+} while ( c <= 6 )*/
+//-----------------------------
+/*for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}*/
+//-----------------------------
+/*for (var c=1; c<=1000000;c++) {
+
+}*/
+
+console.log('Vai começar...')
+for (var c = 1; c<=4; c++) {
+        console.log(c)
+}
+console.log('FIM!')
